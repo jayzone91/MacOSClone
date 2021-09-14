a4030d514bc32daf51b2d0d8127c6d621d96d421
jayzone91 <50213036+jayzone91@users.noreply.github.com>
2021-09-14 10:26:54 +0200

    SASS + _includes richtig

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets/images')
     eleventyConfig.addPassthroughCopy('./src/assets/fonts')
     eleventyConfig.addPassthroughCopy('./src/assets/js')
-    eleventyConfig.addWatchTarget('public/assets/css/*.css')
+    eleventyConfig.addWatchTarget('public/css/*.css')
     eleventyConfig.setBrowserSyncConfig({
         files: ['puclic/assets/css/*.css']
     })
