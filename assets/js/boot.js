0292ccec142bfe378a526791de7f99e0a48dfd27
jayzone91 <50213036+jayzone91@users.noreply.github.com>
2021-09-14 10:20:50 +0200

    Alles richtig geschoben

--- assets/js/boot.js
+++
@@ -1,7 +0,0 @@
-function boot() {
-    document.getElementById("bootscreen").style.display="flex";
-    setTimeout(() => {
-        document.getElementById("bootscreen").style.display="none";
-    }, 4500);
-    
-}
---
+++ src/assets/js/boot.js
@@ -0,0 +1,7 @@
+function boot() {
+    document.getElementById("bootscreen").style.display="flex";
+    setTimeout(() => {
+        document.getElementById("bootscreen").style.display="none";
+    }, 4500);
+    
+}
